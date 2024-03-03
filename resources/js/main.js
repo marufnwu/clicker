2dd321023aa84145bf386633370b1ffca6198765
Mehedi ahsan <mehediahsankhulna@gmail.com>
2024-03-03 13:14:26 +0600

    password toggle fixed

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,8 +8,11 @@ btn.addEventListener("click", function () {
 
 document.addEventListener("DOMContentLoaded", function() {
     const eyeOpen = document.getElementById("eyeOpen");
+    const eyeOpenConfirm = document.getElementById("eyeOpenConfirm");
     const eyeClose = document.getElementById("eyeClose");
+    const eyeCloseConfirm = document.getElementById("eyeCloseConfirm");
     const passwordInput = document.getElementById("password");
+    const passwordInputConfirm = document.getElementById("passwordConfirm");
 
     eyeOpen.addEventListener("click", function() {
         passwordInput.type = "text";
@@ -22,7 +25,38 @@ document.addEventListener("DOMContentLoaded", function() {
         eyeOpen.classList.remove("hidden");
         eyeClose.classList.add("hidden");
     });
+
+    eyeOpenConfirm.addEventListener("click", function() {
+        passwordInputConfirm.type = "text";
+        eyeOpenConfirm.classList.add("hidden");
+        eyeCloseConfirm.classList.remove("hidden");
+    });
+
+    eyeCloseConfirm.addEventListener("click", function() {
+        passwordInputConfirm.type = "password";
+        eyeOpenConfirm.classList.remove("hidden");
+        eyeCloseConfirm.classList.add("hidden");
+    });
+
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const confirmEyeOpen = document.getElementById("confirmEyeOpen");
+    const confirmEyeClose = document.getElementById("confirmEyeClose");
+    const confirmPasswordInput = document.getElementById("password_confirmation");
+
+    confirmEyeOpen.addEventListener("click", function() {
+        confirmPasswordInput.type = "text";
+        confirmEyeOpen.classList.add("hidden");
+        confirmEyeClose.classList.remove("hidden");
+    });
+
+    confirmEyeClose.addEventListener("click", function() {
+        confirmPasswordInput.type = "password";
+        confirmEyeOpen.classList.remove("hidden");
+        confirmEyeClose.classList.add("hidden");
+    });
+});
+
 
 
 
